1828facfc0dd38c67dbc1b82ff5b2e2322071a67
Pratham Rangunwala <prathamdj777@gmail.com>
2023-12-15 11:43:45 +0530

    Final commit for Assignment 1

--- SEM-4/Javascript/ASSIGNMENT - 1/script.js
+++ SEM-4/Javascript/ASSIGNMENT - 1/script.js
@@ -1,18 +1,54 @@
+let displayValue = "";
 
-function LoginAlert(){
-    let name=prompt("Enter your Name: ");
-    alert("Login Successfull");
+function appendToDisplay(value) {
+  displayValue += value;
+  document.getElementById("display").value = displayValue;
 }
 
-function formChanged() {
-    var name = document.getElementsByName("name").value;
-    var address = document.getElementsByName("address").value;
-    var contact = document.getElementsByName("contact").value;
-    var gender = document.getElementsByName("gender").value;
-    var email = document.getElementsByName("email").value;
-    var username = document.getElementsByName("username").value;
-    var password = document.getElementsByName("password").value;    
+function clearDisplay() {
+  displayValue = "";
+  document.getElementById("display").value = displayValue;
 }
 
+function promptRadius() {
+  const radius = prompt("Please enter the radius:");
+  if (radius !== null) {
+    appendToDisplay('radius = ' + radius);
+  }
+}
 
+function confirmDollar() {
+  const dollars = prompt("Please value in dollars:");
+  const confirmed = confirm("Are you sure you want to convert "+dollars+" dollar to INR ?");
+  if (dollars !== null && confirmed) {
+    appendToDisplay('$' + dollars);
+  }
+}
 
+function calculate() {
+  try {
+    if (displayValue.includes('radius = ')) {
+      const radius = parseFloat(displayValue.replace('radius = ', ''));
+      const result = Math.PI * radius * radius;
+      displayValue = parseFloat(result.toFixed(10)).toString();
+      displayValue="Area = "+displayValue;
+      document.getElementById("display").value = displayValue;
+    }else if(displayValue.includes('$')){
+      const dollars = parseFloat(displayValue.replace('$', ''));
+      const rupees = 83 * dollars;
+      displayValue = parseFloat(rupees.toFixed(10)).toString();
+      displayValue="₹"+displayValue;
+      document.getElementById("display").value =  displayValue;
+    } 
+    else {
+      result = new Function('return ' + displayValue)();
+      displayValue = parseFloat(result.toFixed(10)).toString();
+      document.getElementById("display").value = displayValue;
+    }
+    document.getElementById("display").value = displayValue;
+  } catch (error) {
+    displayValue = "";
+    document.getElementById("display").value = "Error";
+    alert("Error")
+  }
+}
